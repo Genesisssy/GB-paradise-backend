215d42ab3e7e319a8e7b4f93ec37fe25e884c59e
Genesis <enelialmanzar@gmail.com>
2026-06-12 02:48:21 -0400

    Preparando Backend para el Render

--- app/main.py
+++ app/main.py
@@ -1,43 +1,10 @@
 from fastapi import FastAPI
 from app.database import engine, Base
-from app import models, crud, schemas
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
-
-# ======================
-# HOME
-# ======================
 @app.get("/")
 def home():
     return {"message": "GB Paradise API 🚀"}
-
-
-# ======================
-# OFFERS
-# ======================
-@app.get("/offers")
-def read_offers():
-    return crud.get_offers()
-
-
-# ======================
-# RESERVATIONS
-# ======================
-@app.get("/reservations")
-def read_reservations():
-    return crud.get_reservations()
-
-
-@app.post("/reservations")
-def add_reservation(reservation: schemas.ReservationSchema):
-    return crud.create_reservation(reservation)
-
-
-from app.seed import seed_offers
-
-@app.on_event("startup")
-def startup():
-    seed_offers()
